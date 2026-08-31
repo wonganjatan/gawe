0b80da92a1e00d65a0fb8b2cd3bf6011e095d133
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-08-31 14:26:45 +1000

    feat: add simple Landing page

---
+++ frontend/src/pages/Landing.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Landing() {
+    return (
+        <div>
+            <h1>Gawe</h1>
+            <Link to="/login">Log in</Link>
+        </div>
+    )
+}
