314eeb1c2a48ab0c068d664af4648a676bddeb22
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-05 13:15:09 +1000

    chore(frontend): rename Landing page to Home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import Landing from "./pages/Landing";
+import Home from "./pages/Home";
+import Layout from "./components/Layout";
+import SignUp from "./pages/auth/SignUp";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Landing/>}/>
+      <Route path="/register" element={<Layout><SignUp/></Layout>}/>
+      <Route path="/" element={<Layout><Home/></Layout>}/>
     </Routes>
   )
 }
