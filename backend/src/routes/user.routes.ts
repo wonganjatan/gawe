fb451184eab2313cfbda66e83808c0b9cdc7e2bb
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-05 12:36:23 +1000

    feat(backend): add create api call

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -13,4 +13,8 @@ router.get("/users", async (req, res) => {
     await userController.findAll(req, res);
 });
 
+router.post("/users/new", async (req, res) => {
+    await userController.create(req, res)
+})
+
 export default router;
