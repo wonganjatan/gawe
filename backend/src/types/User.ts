8d52e03b8b4a426595ff09fc6430e5b55e77d43f
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-05 16:37:46 +1000

    feat(backend): add User and database input dto

--- backend/src/types/User.ts
+++ backend/src/types/User.ts
@@ -8,13 +8,32 @@ export interface User {
     email: string
     hashedPassword: string
     role: Role
-    createdAt: string
+    createdAt: Date
 }
 
-export interface CreateUserRequest {
+export interface UserResponse {
+    id: number
+    firstName: string
+    lastName: string
+    username: string
+    email: string
+    role: Role
+    createdAt: Date
+}
+
+export interface CreateUserInput {
+    firstName: string
+    lastName: string
+    username: string
+    email: string
+    password: string
+}
+
+export interface CreateUserDatabaseInput {
     firstName: string
     lastName: string
     username: string
     email: string
     hashedPassword: string
+    role?: Role
 }
