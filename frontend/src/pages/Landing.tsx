b8c440f53e1b27e1ee643af24401bd01321dcbbb
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-05 12:29:56 +1000

    fix(frontend): add useEffect to get all users

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,10 +1,33 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import type { User } from "../types/User";
+import { usersApi } from "../api/users";
 
 export default function Landing() {
+    const [users, setUsers] = useState<User[] | null>(null)
+
+    useEffect(() => {
+        const getAllUsers = async () => {
+            try {
+                const data = await usersApi.getAllUsers()
+                setUsers(data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        getAllUsers()
+    }, [])
+
     return (
         <div>
             <h1>Gawe</h1>
-            <Link to="/login">Log in</Link>
+            {users && (
+                <ul>
+                    {users.map(user => (
+                        <li key={user.id}>{user.name} - {user.email}</li>
+                    ))}
+                </ul>
+            )}
         </div>
     )
 }
