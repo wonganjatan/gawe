0340ddda7284be01f939a7b968cc7f42e9f06c30
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-06 11:15:26 +1000

    chore(backend): rename user.routes.ts to users.routes.ts in the sake of naming convention

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -13,8 +13,4 @@ router.get("/users", async (req, res) => {
     await userController.findAll(req, res);
 });
 
-router.post("/users/new", async (req, res) => {
-    await userController.create(req, res)
-})
-
 export default router;
