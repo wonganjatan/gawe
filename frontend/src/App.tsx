4a33424fa38d7a25ac9ab7b73ffac4dda6509f92
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-06 11:42:20 +1000

    feat(frontend): add SignIn route to the App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
 import SignUp from "./pages/auth/SignUp";
+import SignIn from "./pages/auth/SignIn";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/register" element={<Layout><SignUp/></Layout>}/>
+      <Route path="/login" element={<Layout><SignIn/></Layout>}/>
       <Route path="/" element={<Layout><Home/></Layout>}/>
     </Routes>
   )
