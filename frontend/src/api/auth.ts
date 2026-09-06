996576e442ac767c9f67d8fe43fdd5f471ff842b
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-06 11:20:58 +1000

    feat(frontend): add sign up api in auth routes

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -5,29 +5,4 @@ export const authApi = {
   register: async (input: CreateUserInput): Promise<void> => {
     await axios.post<void>("/auth/register", input);
   },
-
-  login: async (email: string, password: string): Promise<User> => {
-    const res = await axios.post<User>("/auth/login", { email, password })
-    return res.data
-  }
-
-//   getUserById: async (userId: number) => {
-//     const response = await axios.get(`/users/${userId}`);
-//     return response.data;
-//   },
-
-//   createUser: async (user: Partial<User>) => {
-//     const response = await axios.post("/users", user);
-//     return response.data;
-//   },
-
-//   updateUser: async (userId: number, user: Partial<User>) => {
-//     const response = await axios.put(`/users/${userId}`, user);
-//     return response.data;
-//   },
-
-//   deleteUser: async (userId: number) => {
-//     const response = await axios.delete(`/users/${userId}`);
-//     return response.data;
-//   },
 };
