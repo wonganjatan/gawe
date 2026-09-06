67bd32b8e88e86f1ff97b1c4c912c6dd0804b4f0
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-06 11:41:23 +1000

    feat: add auth api in both frontend and backend

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -13,4 +13,8 @@ router.post("/auth/register", async (req, res) => {
     await authController.register(req, res);
 });
 
+router.post("/auth/login", async (req, res) => {
+    await authController.login(req, res)
+})
+
 export default router;
--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -5,4 +5,9 @@ export const authApi = {
   register: async (input: CreateUserInput): Promise<void> => {
     await axios.post<void>("/auth/register", input);
   },
+
+  login: async (email: string, password: string): Promise<User> => {
+    const res = await axios.post<User>("/auth/login", { email, password })
+    return res.data
+  }
 };
