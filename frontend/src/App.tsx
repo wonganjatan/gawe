476a420cf50735ff35fb12cdbda7720115208edd
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-08 11:02:55 +1000

    feat(frontend): wrap users page to the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Layout from "./components/Layout";
 import SignUp from "./pages/auth/SignUp";
 import SignIn from "./pages/auth/SignIn";
 import { useAuthContext } from "./context/AuthContext";
+import Users from "./pages/user/Users";
 
 export default function App() {
   const { loggedInUser } = useAuthContext()
@@ -13,6 +14,7 @@ export default function App() {
       <Route path="/" element={loggedInUser ? <Layout><Home/></Layout> : <Navigate to="/login" replace/>}/>
       <Route path="/register" element={loggedInUser ? <Navigate to="/" replace/> : <Layout><SignUp/></Layout>}/>
       <Route path="/login" element={loggedInUser ? <Navigate to="/" replace/> : <Layout><SignIn/></Layout>}/>
+      <Route path="/users" element={loggedInUser ? <Layout><Users/></Layout> : <Navigate to="/login" replace/>}/>
     </Routes>
   )
 }
