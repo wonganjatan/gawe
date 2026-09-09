58ead98fb29539b01e097ae936fa403538d6a8f2
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-09 14:54:16 +1000

    feat(frontend): add ProjectForm dto o create project

--- frontend/src/types/Project.ts
+++ frontend/src/types/Project.ts
@@ -13,3 +13,15 @@ export interface Project {
     tasks?: Task[]
     createdAt: string
 }
+
+export interface ProjectForm {
+    name: string
+    description?: string
+    status: "Planning" | "In Progress" | "Completed"
+    ownerId: number
+    memberIds?: number[]
+    startDate: string
+    dueDate: string
+    completedAt: string | null
+    tasks?: Task[]
+}
