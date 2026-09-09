39f3261ac198786ad200ee6d89514733d8222fa9
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-09 15:13:43 +1000

    feat(frontend): add Projects page placeholder

---
+++ frontend/src/pages/project/Projects.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Projects() {
+    
+    return (
+        <div>
+            <Link to="/projects/new">New Project</Link>
+        </div>
+    )
+}
