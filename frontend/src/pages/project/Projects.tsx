9787db6b7eab7db3144db792b106aa8d84c44e47
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-10 10:21:32 +1000

    feat(frontend): fetch all projects

--- frontend/src/pages/project/Projects.tsx
+++ frontend/src/pages/project/Projects.tsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import type { Project } from "../../types/Project";
+import { projectsApi } from "../../api/projects";
 
 export default function Projects() {
+    const [projects, setProjects] = useState<Project[]>()
+
+    useEffect(() => {
+        const findAll = async () => {
+            try {
+                const projects = await projectsApi.getAll()
+                console.log(projects)
+                setProjects(projects)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        findAll()
+    }, [])
     
     return (
         <div>
             <Link to="/projects/new">New Project</Link>
+            {projects?.map(project => (
+                <div key={project.id}>
+                    <p>{project.name}</p>
+                </div>
+            ))}
         </div>
     )
 }
