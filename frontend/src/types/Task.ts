b4668344745a1b2d76bba26657804c27693836e7
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-14 13:47:06 +1000

    fix(frontend): align task status value to match with prisma

--- frontend/src/types/Task.ts
+++ frontend/src/types/Task.ts
@@ -2,7 +2,7 @@ export interface Task {
     id: number
     title: string
     description?: string
-    status: "Todo" | "In Progress" | "Done"
+    status: "Todo" | "InProgress" | "Done"
     priority?: "Low" | "Medium" | "High"
     projectId: number
     assignedId?: number
