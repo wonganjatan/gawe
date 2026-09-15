6c3171470d5f599ff129bb8f66bdbafe0dc00180
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-16 09:44:47 +1000

    fix(frontend): change the status value In Progress to InProgress to mastch with prisma schema

--- frontend/src/types/Task.ts
+++ frontend/src/types/Task.ts
@@ -27,7 +27,7 @@ export interface TaskResponse {
 export interface TaskForm {
     title: string
     description?: string
-    status: "Todo" | "In Progress" | "Done"
+    status: "Todo" | "InProgress" | "Done"
     priority?: "Low" | "Medium" | "High"
     projectId: number
     assignedId?: number
