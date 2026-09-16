ab6f236df965651d786751eaa36b28ea05c87312
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-16 15:24:33 +1000

    feat(backend): add project update service layer

--- backend/src/modules/project/IProjectService.ts
+++ backend/src/modules/project/IProjectService.ts
@@ -1,8 +1,9 @@
-import { ProjectCreateForm, ProjectResponse } from "../../types/Project";
+import { ProjectCreateForm, ProjectResponse, ProjectUpdateForm } from "../../types/Project";
 
 export interface IProjectService {
     findAll(): Promise<ProjectResponse[]>
     findById(id: number): Promise<ProjectResponse | null>
     create(form: ProjectCreateForm): Promise<void>
+    update(id: number, data: ProjectUpdateForm): Promise<ProjectResponse>
     delete(id: number): Promise<void>
 }
