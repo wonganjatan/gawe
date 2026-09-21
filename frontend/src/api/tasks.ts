b2db48c3524175345f3dd00c73c0fd41edea29e5
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-21 11:41:41 +1000

    feat(frontend): add getAll method for tasks

--- frontend/src/api/tasks.ts
+++ frontend/src/api/tasks.ts
@@ -2,6 +2,11 @@ import type { Task } from "../types/Task"
 import axios from "./axios"
 
 export const tasksApi = {
+    getAll: async (): Promise<Task[]> => {
+        const response = await axios.get<Task[]>("/tasks");
+        return response.data;
+    },
+
     updateStatus: async (id: number, status: Task["status"]): Promise<void> => {
         await axios.put(`/tasks/${id}`, { status })
     }
