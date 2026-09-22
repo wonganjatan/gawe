cbcd041a6ae3fa24cc42f6d24b44e4c8dd59eb58
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-22 18:26:54 +1000

    feat(frontend): add specific task id page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import ProjectCreate from "./pages/project/ProjectCreate";
 import ProjectDetails from "./pages/project/ProjectDetails";
 import ProjectUpdate from "./pages/project/ProjectUpdate";
 import Tasks from "./pages/task/Tasks";
+import TaskDetails from "./pages/task/TaskDetails";
 
 export default function App() {
   const { loggedInUser, loading } = useAuthContext()
@@ -29,6 +30,7 @@ export default function App() {
       <Route path="/projects/:id" element={loggedInUser ? <Layout><ProjectDetails/></Layout> : <Navigate to="/login" replace/>}/>
       <Route path="/projects/:id/edit" element={loggedInUser ? <Layout><ProjectUpdate/></Layout> : <Navigate to="/login" replace/>}/>
       <Route path="/tasks" element={loggedInUser ? <Layout><Tasks/></Layout> : <Navigate to="/login" replace/>}/>
+      <Route path="/tasks/:id" element={loggedInUser ? <Layout><TaskDetails/></Layout> : <Navigate to="/login" replace/>}/>
     </Routes>
   )
 }
