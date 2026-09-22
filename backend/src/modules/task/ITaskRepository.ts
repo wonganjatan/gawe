f28d2134a70855e8a5295c2d059e8b432b87aad7
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-22 11:55:32 +1000

    feat(backend): add get all tasks method to repo layer

--- backend/src/modules/task/ITaskRepository.ts
+++ backend/src/modules/task/ITaskRepository.ts
@@ -1,5 +1,6 @@
-import { Task } from "../../types/Task";
+import { Task, TaskResponse } from "../../types/Task";
 
 export interface ITaskRepository {
+    findAll(): Promise<TaskResponse[]>
     updateStatus(id: number, newStatus: Task["status"]): Promise<void>
 }
--- backend/src/modules/task/TaskRepository.ts
+++ backend/src/modules/task/TaskRepository.ts
@@ -1,8 +1,25 @@
 import { prisma } from "../../lib/prisma";
-import { Task } from "../../types/Task";
+import { Task, TaskResponse } from "../../types/Task";
 import { ITaskRepository } from "./ITaskRepository";
 
 export class TaskRepository implements ITaskRepository {
+    async findAll(): Promise<TaskResponse[]> {
+        return await prisma.task.findMany({
+            select: {
+                id: true,
+                title: true,
+                description: true,
+                status: true,
+                priority: true,
+                projectId: true,
+                assignedId: true,
+                dueDate: true,
+                completedAt: true,
+                createdAt: true
+            }
+        })
+    }
+
     async updateStatus(id: number, newStatus: Task["status"]): Promise<void> {
         await prisma.task.update({
             where: { id },
