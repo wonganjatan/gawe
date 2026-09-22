21185ca939ab5ca9674b975a738cbe15805f9624
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-22 11:56:05 +1000

    feat(backend): add get all tasks method to controller layer

--- backend/src/modules/task/TaskController.ts
+++ backend/src/modules/task/TaskController.ts
@@ -1,3 +1,4 @@
+import { Request, Response } from 'express';
 import { Task } from "../../../generated/prisma/browser";
 import { ITaskService } from "./ITaskService";
 
@@ -8,7 +9,26 @@ export class TaskController {
         this.taskService = taskService
     }
 
-    async updateStatus(id: number, newStatus: Task["status"]): Promise<void> {
-        await this.taskService.updateStatus(id, newStatus)
+    async findAll(req: Request, res: Response) {
+        try {
+            const tasks = await this.taskService.findAll()
+            return res.json(tasks)
+        } catch (error) {
+            console.error(error)
+            return res.status(500).json({ message: "Failed to fetch Tasks" })
+        } 
+    }
+
+    async updateStatus(req: Request, res: Response): Promise<void> {
+        const id = Number(req.params.id)
+        const { status } = req.body
+        
+        try {
+            await this.taskService.updateStatus(id, status)
+            res.status(200).json({ message: `Task ${id} status successfully updated` })
+        } catch (error) {
+            console.error(error)
+            res.status(500).json({ message: `Failed to update Task ${id}` })
+        }
     }
 }
